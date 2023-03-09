8bd397bc5b8491c422c5d72105247068dce0f70d
Durgesh Wagh <121015119+waghdurgesh@users.noreply.github.com>
2023-03-10 01:48:46 +0530

    hide

--- CEPBackEnd_Multi_JWT/src/main/java/com/app/custom_exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.app.custom_exception;
-
-@SuppressWarnings("serial")
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String mesg) {
-		super(mesg);
-	}
-}
--- CEPBackEnd_Multi_JWT/src/main/java/com/app/repository/IQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entity.Question;
-
-public interface IQuestionRepository extends JpaRepository<Question, Long> {
-
-}
